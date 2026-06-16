8bc0bbc4452572ceb8f3bbf6d4cd7351bff5a1be
Ariyan Akash <ariyanakash01303@gmail.com>
2026-06-16 12:44:20 +0600

    banner and time set

--- src/components/layouts/Banner.jsx
+++ src/components/layouts/Banner.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import Container from './Container'
 import { Link } from 'react-router';
 import { FaAngleRight } from "react-icons/fa";
-
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import banner from "../../assets/banner.webp";
+import { Pagination,Autoplay } from 'swiper/modules';
+import 'swiper/css/pagination';
 const Banner = () => {
   return (
     <>
     <div className=' border border-b-2 border-[#dad7d7] mt-5 mb-10'></div>
     <Container>
         <div className='flex '>
-            <div className='max-w-[300px]'>
+            <div className='max-w-[300px] '>
                 <ul>
                    <li className='mb-4 font-pop text-[16px] text-[#000000]'>
                      <Link className='flex items-center '>Woman’s Fashion <span className='ml-13'><FaAngleRight /></span></Link>
@@ -27,8 +30,25 @@ const Banner = () => {
                     <li className='mb-4 font-pop text-[16px] text-[#000000]'>Health & Beauty</li>
                 </ul>
             </div>
-            <div>
-                
+          <div className='max-w-[892px] mx-auto ml-[61px]'>
+               <Swiper
+                spaceBetween={0}
+                slidesPerView={1} 
+                autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+               }}
+               pagination={{
+               clickable: true,
+               }}
+                modules={[Pagination,Autoplay]}
+                >
+                <SwiperSlide><img src={banner} alt="banner" /></SwiperSlide>
+                <SwiperSlide><img src={banner} alt="banner" /></SwiperSlide>
+                <SwiperSlide><img src={banner} alt="banner" /></SwiperSlide>
+                <SwiperSlide><img src={banner} alt="banner" /></SwiperSlide>
+                <SwiperSlide><img src={banner} alt="banner" /></SwiperSlide>
+               </Swiper>
             </div>
         </div>
     </Container>
--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router'
 import TopBar from '../layouts/TopBar'
 import Navbar from '../Navbar'
 import Banner from './Banner'
+import FlashSales from '../FlashSeles'
 
 const MainLayout = () => {
   return (
@@ -10,6 +11,7 @@ const MainLayout = () => {
    <TopBar/>
    <Navbar/>
    <Banner/>
+   <FlashSales/>
    <Outlet/>
    </>
   )
