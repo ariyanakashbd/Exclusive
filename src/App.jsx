69f2791a72f5ddfeb6b034cf0390dd2642890d28
Ariyan Akash <ariyanakash01303@gmail.com>
2026-06-24 17:29:57 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router"; 
 import Home from "./pages/Home";
 import SignUp from "./pages/SingUp"; 
+import Login from "./pages/Login";
 import MainLayout from "./components/layouts/MainLayout";
 import Category from "./pages/Category";
 
@@ -10,9 +11,9 @@ function App() {
       <Routes>
       
         <Route element={<MainLayout />}>
-          <Route path="/home" element={<Home />} />
+         <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUp />} /> 
-          <Route path="/login" element={<Login />} />
+          <Route path="/Login" element={<Login />} />
           <Route path="/all-products" element={<Category />} /> 
         </Route>
       </Routes>
